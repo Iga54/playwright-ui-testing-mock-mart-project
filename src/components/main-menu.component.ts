e1ba98fe735e53dd2937fe1785771806893b1199
Iga <igcwiklinska@gmail.com>
2025-05-09 14:44:29 +0200

    feat: create main menu component

---
+++ src/components/main-menu.component.ts
@@ -0,0 +1,18 @@
+import { Locator, Page } from '@playwright/test';
+
+export class MainMenuComponent {
+  siteLogo: Locator;
+  productsLink: Locator;
+  categoriesLink: Locator;
+  userGreeting: Locator;
+  logoutButton: Locator;
+
+  constructor(private page: Page) {
+    this.siteLogo = this.page.getByRole('link', { name: 'Mock Mart' });
+    this.productsLink = this.page.getByRole('link', { name: 'Products' });
+    this.categoriesLink = this.page.getByRole('link', { name: 'Categories' });
+    this.userGreeting = this.page.getByText('Hello, John!');
+    this.logoutButton = this.page.getByRole('button', { name: 'Sign Out' });
+  }
+}
+
