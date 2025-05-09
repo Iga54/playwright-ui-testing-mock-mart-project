67e5087e837104c427408a024986eaf8e7d0c934
Iga <igcwiklinska@gmail.com>
2025-05-09 14:45:10 +0200

    test: add functional login tests

--- tests/functional-tests/login.spec.ts
+++ tests/functional-tests/login.spec.ts
@@ -1,14 +1,46 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { LoginPage } from 'src/pages/login.page';
+import { testUser1 } from 'src/test-data/user-login-data';
 
 test.describe('login verification', async () => {
+  let loginPage: LoginPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.goto();
+  });
+
   test('user can login with correct data', async ({ page }) => {
     //Arrange:
-    const loginPange = new LoginPage(page);
 
     //Act:
-    await loginPange.goto();
-    
+    const homePage = await loginPage.loginUser(testUser1);
+
     //Assert:
+    expect(homePage.mainMenuComponent.userGreeting).toBeVisible();
+  });
+
+  test('user can not login with incorrect email', async ({ page }) => {
+    //Arrange:
+    const expectedErrorMessage = 'Failed to sign in';
+    testUser1.userEmail = 'test23@22.com';
+
+    //Act:
+    await loginPage.loginUser(testUser1);
+
+    //Assert:
+    expect(loginPage.loginErrorMessage).toHaveText(expectedErrorMessage);
+  });
+
+  test('user can not login with incorrect password', async ({ page }) => {
+    //Arrange:
+    const expectedErrorMessage = 'Failed to sign in';
+    testUser1.userPassword = 'test23';
+
+    //Act:
+    await loginPage.loginUser(testUser1);
+
+    //Assert:
+    expect(loginPage.loginErrorMessage).toHaveText(expectedErrorMessage);
   });
 });
