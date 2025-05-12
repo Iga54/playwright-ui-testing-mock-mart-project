89184b48849323cd51d6519e42945f18edf4b945
Iga <igcwiklinska@gmail.com>
2025-05-12 15:41:31 +0200

    feat: add CategoriesPage and HomePage page objects

---
+++ src/pages/categories.page.ts
@@ -0,0 +1,14 @@
+import { Page } from '@playwright/test';
+import { MainMenuComponent } from 'src/components/main-menu.component';
+import { BasePage } from 'src/pages/base.page';
+
+export class CategoriesPage extends BasePage {
+  url = '/categories';
+  mainMenuComponent = new MainMenuComponent(this.page);
+
+  categoriesText = this.page.getByRole('heading', { name: 'Categories' });
+
+  constructor(page: Page) {
+    super(page);
+  }
+}
--- src/pages/home.page.ts
+++ src/pages/home.page.ts
@@ -1,11 +1,30 @@
 import { Page } from '@playwright/test';
 import { MainMenuComponent } from 'src/components/main-menu.component';
 import { BasePage } from 'src/pages/base.page';
+import { LoginPage } from 'src/pages/login.page';
+import { RegisterPage } from 'src/pages/register.page';
 
 export class HomePage extends BasePage {
+  url = '/';
   mainMenuComponent = new MainMenuComponent(this.page);
+
+  homePageText = this.page.getByRole('heading', {
+    name: 'Featured Products',
+  });
 
   constructor(page: Page) {
     super(page);
+  }
+
+  async goToLoginPage() {
+    await this.mainMenuComponent.signInLink.click();
+
+    return new LoginPage(this.page);
+  }
+
+  async goToRegisterPage() {
+    await this.mainMenuComponent.signUpLink.click();
+
+    return new RegisterPage(this.page);
   }
 }
