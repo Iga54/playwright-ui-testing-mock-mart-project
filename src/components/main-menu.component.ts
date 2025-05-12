06fffe00cbd5b4c4acdf57ba02c27e7eefda0d17
Iga <igcwiklinska@gmail.com>
2025-05-12 15:38:29 +0200

    feat: extend main menu component with new navigation items

--- src/components/main-menu.component.ts
+++ src/components/main-menu.component.ts
@@ -6,6 +6,8 @@ export class MainMenuComponent {
   categoriesLink: Locator;
   userGreeting: Locator;
   logoutButton: Locator;
+  signInLink: Locator;
+  signUpLink: Locator;
 
   constructor(private page: Page) {
     this.siteLogo = this.page.getByRole('link', { name: 'Mock Mart' });
@@ -13,6 +15,7 @@ export class MainMenuComponent {
     this.categoriesLink = this.page.getByRole('link', { name: 'Categories' });
     this.userGreeting = this.page.getByText('Hello, John!');
     this.logoutButton = this.page.getByRole('button', { name: 'Sign Out' });
+    this.signInLink = this.page.getByRole('link', { name: 'Sign In' });
+    this.signUpLink = this.page.getByRole('link', { name: 'Sign Up' });
   }
 }
-
