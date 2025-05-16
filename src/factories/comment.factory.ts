885293ac5b12b8b6669a06b51c24a90228639474
Iga <igcwiklinska@gmail.com>
2025-05-16 18:03:48 +0200

    refactor: change function name in comment factory

--- src/factories/comment.factory.ts
+++ src/factories/comment.factory.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker/locale/en';
 import { CommentModel } from 'src/models/comment.model';
 
-export function createComment(bodySentence = 5): CommentModel {
+export function prepareNewComment(bodySentence = 5): CommentModel {
   const body = faker.lorem.sentence(bodySentence);
 
   const newComment: CommentModel = { body: body };
