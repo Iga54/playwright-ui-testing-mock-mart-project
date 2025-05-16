ab0815ffe24c9fe4a297b8718cb62a7da2f8d446
Iga <igcwiklinska@gmail.com>
2025-05-16 16:36:29 +0200

    feat: add registerPage object

--- src/pages/register.page.ts
+++ src/pages/register.page.ts
@@ -1,10 +1,12 @@
 import { Page } from '@playwright/test';
+import { MainMenuComponent } from 'src/components/main-menu.component';
 import { RegisterUserModel } from 'src/models/user.models';
 import { BasePage } from 'src/pages/base.page';
 import { LoginPage } from 'src/pages/login.page';
 
 export class RegisterPage extends BasePage {
   url = '/signup';
+  mainMenu = new MainMenuComponent(this.page);
 
   userNameInput = this.page.getByRole('textbox', { name: 'Full Name' });
   userEmailInput = this.page.getByRole('textbox', { name: 'Email address' });
@@ -15,7 +17,16 @@ export class RegisterPage extends BasePage {
   userConfirmPasswordInput = this.page.getByRole('textbox', {
     name: 'Confirm Password',
   });
+
   registerBtn = this.page.getByRole('button', { name: 'Create account' });
+  signUpText = this.page.getByRole('heading', { name: 'Create an Account' });
+
+  errorPasswordMismatchMessage = this.page.getByText('Passwords do not match');
+  errorPasswordTooShortMessage = this.page.getByText(
+    'Password must be at least 6',
+  );
+  errorPasswordTooLongMessage = this.page.getByText('Passwords do not match');
+  errorPasswordEmojis = this.page.getByText('Name must not contain emojis');
 
   constructor(page: Page) {
     super(page);
@@ -25,7 +36,9 @@ export class RegisterPage extends BasePage {
     await this.userNameInput.fill(registerUserData.userName);
     await this.userEmailInput.fill(registerUserData.userEmail);
     await this.userPasswordInput.fill(registerUserData.userPassword);
-    await this.userConfirmPasswordInput.fill(registerUserData.userPassword);
+    await this.userConfirmPasswordInput.fill(
+      registerUserData.userConfirmPassword,
+    );
     await this.registerBtn.click();
     return new LoginPage(this.page);
   }
