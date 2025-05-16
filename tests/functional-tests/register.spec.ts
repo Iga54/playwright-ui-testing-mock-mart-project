aa598beb0c874fa73660bdd6b0d4b127f5d4b83d
Iga <igcwiklinska@gmail.com>
2025-05-16 16:35:34 +0200

    test: add functional tests for user registration

--- tests/functional-tests/register.spec.ts
+++ tests/functional-tests/register.spec.ts
@@ -6,28 +6,105 @@ import { RegisterPage } from 'src/pages/register.page';
 test.describe('verification of user registration', async () => {
   let registerPage: RegisterPage;
   let registerUserData: RegisterUserModel;
+
   test.beforeEach(async ({ page }) => {
     registerPage = new RegisterPage(page);
     await registerPage.goto();
+    registerUserData = createRandomUser();
   });
 
-  test('user can register with correct data', async ({ page }) => {
+  test('happy path - user can register with correct data', async ({ page }) => {
     //Act:
-    registerUserData = createRandomUser();
     const loginPage = await registerPage.registerUser(registerUserData);
-    await loginPage.waitForPageToLoadUrl();
 
     //Assert:
-    expect(loginPage.signInText).toBeVisible();
+    await expect(loginPage.signInText).toBeVisible();
   });
-  test('user can not register - empty email', async ({ page }) => {
-    //Act:
-    registerUserData = createRandomUser();
+
+  test('negative path - user can not register with empty email', async ({}) => {
+    //Arrange:
+    const expectedErrorText = 'Please fill in this field.';
     registerUserData.userEmail = '';
-    const loginPage = await registerPage.registerUser(registerUserData);
-    await loginPage.waitForPageToLoadUrl();
+    const emailInput = registerPage.userEmailInput;
+    const errorEmailMessage = await emailInput.evaluate(
+      (el: HTMLInputElement) => el.validationMessage,
+    );
+
+    //Act:
+    await registerPage.registerUser(registerUserData);
 
     //Assert:
-    expect(loginPage.signInText).toBeVisible();
+    expect(errorEmailMessage).toBe(expectedErrorText);
+  });
+
+  test('negative path - user can not register with incorrect email form', async ({}) => {
+    //Arrange:
+    const expectedErrorText = 'Please fill in this field.';
+    registerUserData.userEmail = 'test2.com';
+    const emailInput = registerPage.userEmailInput;
+    const errorEmailMessage = await emailInput.evaluate(
+      (el: HTMLInputElement) => el.validationMessage,
+    );
+
+    ///Act:
+    await registerPage.registerUser(registerUserData);
+
+    //Assert:
+    expect(errorEmailMessage).toBe(expectedErrorText);
+  });
+
+  test('negative path - user can not register with inconsistent passwords', async ({}) => {
+    //Arrange:
+    const expectedErrorText = 'Passwords do not match';
+    registerUserData.userPassword = '111111';
+
+    //Act:
+    await registerPage.registerUser(registerUserData);
+
+    //Assert:
+    await expect(registerPage.errorPasswordMismatchMessage).toHaveText(
+      expectedErrorText,
+    );
+  });
+
+  test('negative path - user can not register with too short password', async ({}) => {
+    //Arrange:
+    const expectedErrorText = 'Password must be at least 6 characters long';
+    const shortPassword = (registerUserData.userPassword = '12345');
+    registerUserData.userConfirmPassword = shortPassword;
+
+    //Act:
+    await registerPage.registerUser(registerUserData);
+
+    //Assert:
+    await expect(registerPage.errorPasswordTooShortMessage).toHaveText(
+      expectedErrorText,
+    );
+  });
+
+  test('negative path - user can not register with too long password', async ({}) => {
+    //Arrange:
+    const expectedErrorText = 'Passwords do not match';
+    registerUserData.userPassword = '1111111';
+
+    //Act:
+    await registerPage.registerUser(registerUserData);
+
+    //Assert:
+    expect(registerPage.errorPasswordTooLongMessage).toHaveText(
+      expectedErrorText,
+    );
+  });
+
+  test('negative path - user can not register with emojis in name input', async ({}) => {
+    //Arrange:
+    const expectedErrorText = 'Name must not contain emojis';
+    registerUserData.userName = '112222:)';
+
+    //Act:
+    await registerPage.registerUser(registerUserData);
+
+    //Assert:
+    expect(registerPage.errorPasswordEmojis).toHaveText(expectedErrorText);
   });
 });
