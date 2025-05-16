597584434fdb1597ccb3ce3a94d5a9de255ffb47
Iga <igcwiklinska@gmail.com>
2025-05-16 18:00:52 +0200

    feat: extend main menu component with new methods

--- src/components/main-menu.component.ts
+++ src/components/main-menu.component.ts
@@ -2,10 +2,12 @@ import { Locator, Page } from '@playwright/test';
 import { CategoriesPage } from 'src/pages/categories.page';
 import { HomePage } from 'src/pages/home.page';
 import { LoginPage } from 'src/pages/login.page';
+import { ProductsPage } from 'src/pages/products.page';
 import { RegisterPage } from 'src/pages/register.page';
 
 export class MainMenuComponent {
   siteLogo: Locator;
+  homePageLink: Locator;
   productsLink: Locator;
   categoriesLink: Locator;
   userGreeting: Locator;
@@ -15,7 +17,14 @@ export class MainMenuComponent {
 
   constructor(private page: Page) {
     this.siteLogo = this.page.getByRole('link', { name: 'Mock Mart' });
-    this.productsLink = this.page.getByRole('link', { name: 'Products' });
+    this.homePageLink = this.page.getByRole('link', {
+      name: 'Home',
+      exact: true,
+    });
+    this.productsLink = page.getByRole('link', {
+      name: 'Products',
+      exact: true,
+    });
     this.categoriesLink = this.page.getByRole('link', { name: 'Categories' });
     this.userGreeting = this.page.getByText('Hello, John!');
     this.logoutButton = this.page.getByRole('button', { name: 'Sign Out' });
@@ -34,8 +43,14 @@ export class MainMenuComponent {
     return new RegisterPage(this.page);
   }
 
-  async goToHomePage() {
+  async goToHomePageLogo() {
     await this.siteLogo.click();
+
+    return new HomePage(this.page);
+  }
+
+  async goToHomePageLink() {
+    await this.homePageLink.click();
 
     return new HomePage(this.page);
   }
@@ -44,5 +59,11 @@ export class MainMenuComponent {
     await this.categoriesLink.click();
 
     return new CategoriesPage(this.page);
+  }
+
+  async goToProductsPage() {
+    await this.productsLink.click();
+
+    return new ProductsPage(this.page);
   }
 }
