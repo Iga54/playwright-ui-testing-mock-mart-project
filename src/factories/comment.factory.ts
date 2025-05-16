b03434d373c35920de402e67bc7a57750f46a9c0
Iga <igcwiklinska@gmail.com>
2025-05-16 15:28:14 +0200

    test: add comment model and factory for test data

---
+++ src/factories/comment.factory.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker/locale/en';
+import { CommentModel } from 'src/models/comment.model';
+
+export function createComment(bodySentence = 5): CommentModel {
+  const body = faker.lorem.sentence(bodySentence);
+
+  const newComment: CommentModel = { body: body };
+
+  return newComment;
+}
