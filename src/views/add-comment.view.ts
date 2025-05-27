b990c1afe7622ba5e0e077244028c281684bdf4f
Iga <igcwiklinska@gmail.com>
2025-05-27 16:34:06 +0200

    feat: add AddCommentView component

---
+++ src/views/add-comment.view.ts
@@ -0,0 +1,25 @@
+import { Locator, Page } from '@playwright/test';
+import { CommentModel } from 'src/models/comment.model';
+
+export class AddCommentView {
+  bodyInput: Locator;
+  postCommentButton: Locator;
+  addCommentViewTitle: Locator;
+
+  constructor(private page: Page) {
+    this.bodyInput = this.page.getByRole('textbox', {
+      name: 'Write your comment here...',
+    });
+    this.postCommentButton = this.page.getByRole('button', {
+      name: 'Post Comment',
+    });
+    this.addCommentViewTitle = this.page.getByRole('heading', {
+      name: 'Add a Comment',
+    });
+  }
+
+  async createComment(commentData: CommentModel): Promise<void> {
+    await this.bodyInput.fill(commentData.body);
+    await this.postCommentButton.click();
+  }
+}
