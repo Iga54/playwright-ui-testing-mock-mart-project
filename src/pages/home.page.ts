e6f90ca63f230cd2e27e3533eb054d770c331d4f
Iga <igcwiklinska@gmail.com>
2025-05-27 16:27:05 +0200

    feat: add new methods and locators to HomePage

--- src/pages/home.page.ts
+++ src/pages/home.page.ts
@@ -1,16 +1,27 @@
 import { Page } from '@playwright/test';
 import { MainMenuComponent } from 'src/components/main-menu.component';
 import { BasePage } from 'src/pages/base.page';
+import { ProductsPage } from 'src/pages/products.page';
 
 export class HomePage extends BasePage {
   url = '/';
   mainMenu = new MainMenuComponent(this.page);
 
   homePageText = this.page.getByRole('heading', {
-    name: 'Featured Products',
+    name: 'Welcome to MockMart',
+  });
+
+  homePageExploreButton = this.page.getByRole('button', {
+    name: 'Explore Our Collection',
   });
 
   constructor(page: Page) {
     super(page);
+  }
+
+  async exploreProductsPage() {
+    this.homePageExploreButton.click();
+
+    return new ProductsPage(this.page);
   }
 }
