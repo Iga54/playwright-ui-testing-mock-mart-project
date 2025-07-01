e740e4811328129819bae5ad45d6f87e61562fba
Iga <igcwiklinska@gmail.com>
2025-07-01 17:47:46 +0200

    chore: adding login setup script with session storage

---
+++ tests/setup/login.setup.ts
@@ -0,0 +1,20 @@
+import { expect, test as setup } from '@playwright/test';
+import { STORAGE_STATE } from 'playwright.config';
+import { LoginPage } from 'src/pages/login.page';
+import { testUser1 } from 'src/test-data/user-login-data';
+
+setup('login and save session', async ({ page }) => {
+  //Arrange:
+  const expectedGreeting = '';
+  const loginPage = new LoginPage(page);
+
+  //Act:
+  await loginPage.goto();
+  const homePage = await loginPage.loginUser(testUser1);
+
+  //Assert:
+  await expect(homePage.homePageText).toBeVisible();
+  await expect(homePage.mainMenu.userGreeting).toHaveText(expectedGreeting);
+
+  await page.context().storageState({ path: STORAGE_STATE });
+});
