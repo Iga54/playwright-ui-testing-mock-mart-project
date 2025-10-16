61eb2ddcacfb9cbf0ce410374603b5089ef78a07
Iga <igcwiklinska@gmail.com>
2025-10-16 19:23:57 +0200

    test: add api-login.setup.ts

---
+++ tests/api/setup/api-login.setup.ts
@@ -0,0 +1,23 @@
+// auth.setup.ts
+import { expect, test as setup } from '@playwright/test';
+import { userSignUpData } from 'src/test-data/api-signup-data';
+
+const authFile = 'tmp/auth.json';
+
+setup('authenticates a user and creates a session', async ({ request }) => {
+  // Arrange:
+  const expectedStatusCode = 200;
+
+  //Act:
+  const response = await request.post('/api/auth/signin', {
+    data: {
+      email: userSignUpData.email,
+      password: userSignUpData.password,
+    },
+  });
+
+  //Assert:
+  expect(response.status()).toBe(expectedStatusCode);
+
+  await request.storageState({ path: authFile });
+});
